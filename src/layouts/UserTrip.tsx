0cc52ee6d21cb2776e43ec89d810d92cb3296f9c
Rohith Palakirti <rohithkp99@gmail.com>
2022-09-28 13:24:09 +0000

    Created basic carousel for trip card

--- src/layouts/UserTrip.tsx
+++ src/layouts/UserTrip.tsx
@@ -20,7 +20,14 @@ import DatePicker from "react-datepicker/dist/react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import React, { useEffect, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
-import { Book, Edit, ExternalLink, FaceId, QuestionMark, Refresh, RefreshDot, Tag, Trash, Calendar, Photo, SettingsAutomation, Users } from "tabler-icons-react";
+import { Book, Edit, ExternalLink, FaceId, QuestionMark, Refresh, RefreshDot, Tag, Trash, Calendar, Photo, SettingsAutomation, Users, MapSearch } from "tabler-icons-react";
+import moment from "moment";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+// import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+
 
 import { rescanFaces, trainFaces } from "../actions/facesActions";
 import { scanAllPhotos, scanNextcloudPhotos, scanPhotos } from "../actions/photosActions";
@@ -40,6 +47,7 @@ import { ModalNextcloudScanDirectoryEdit } from "../components/modals/ModalNextc
 import { CountStats } from "../components/statistics";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { ModalAlbumEdit } from "../components/album/ModalAlbumEdit";
+import { JourneyMap } from "./JourneyMap";
 
 export const UserTrip = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -115,164 +123,281 @@ export const UserTrip = () => {
     }
 
     const [date, setDate] = useState(new Date());
-    const [checkOpen, setOpen] = useState(false);
-    const handleChange = (e) => {
-        setIsOpen(!checkOpen);
-        setDate(e);
-    };
-    const handleClick = (e) => {
-        e.preventDefault();
-        setIsOpen(!checkOpen);
+
+    function SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block", background: "gray" }}
+                onClick={onClick}
+            />
+        );
+    }
+
+    function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block", background: "gray" }}
+                onClick={onClick}
+            />
+        );
+    }
+
+    const [clickCard, setClickCard] = useState(false)
+    function handleClickCard() {
+        setClickCard(clickCard => !clickCard);
+    }
+
+    const carouselSettings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        speed: 500,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+
     };
 
     return (
-        <Stack align="center" justify="flex-start">
-            <Group spacing="xs">
-                <Book size={35} />
-                <Title order={2}>{t("sidemenu.usertrip")}</Title>
-            </Group>
+        <div>
+            <Stack align="center" justify="flex-start">
+                <Group spacing="xs">
+                    <Book size={35} />
+                    <Title order={2}>{t("sidemenu.usertrip")}</Title>
+                </Group>
 
-            <Group position="center">
-                <div>
-                    <Title align="center">{countStats.num_photos}</Title>
-                    <Group position="center">
-                        <Button variant="subtle" color="dark" leftIcon={<Photo size={20} />}>
-                            {t("countstats.photos")}
-                        </Button>
-                    </Group>
-                </div>
-
-                <div>
-                    <Title align="center">{countStats.num_people}</Title>
-                    <Group position="center">
-                        <Button variant="subtle" color="dark" leftIcon={<Users size={20} />}>
-                            {t("people")}
-                        </Button>
-                    </Group>
-                </div>
-
-                <div>
-                    <Title align="center">{countStats.num_faces}</Title>
-                    <Group position="center">
-                        <Button variant="subtle" color="dark" leftIcon={<FaceId size={20} />}>
-                            {t("faces")}
-                        </Button>
-                    </Group>
-                </div>
-
-                <div>
-                    <Title align="center">{countStats.num_albumauto}</Title>
-                    <Group position="center">
-                        <Button variant="subtle" color="dark" leftIcon={<SettingsAutomation size={20} />}>
-                            {t("events")}
-                        </Button>
-                    </Group>
-                </div>
-
-                <div>
-                    <Title align="center">{countStats.num_albumdate}</Title>
-                    <Group position="center">
-                        <Button variant="subtle" color="dark" leftIcon={<Calendar size={20} />}>
-                            {t("days")}
-                        </Button>
-                    </Group>
-                </div>
-            </Group >
-
-            {/* <CountStats /> */}
-            {/* <Divider hidden /> */}
-            <Divider />
-            <Box
-                sx={theme => ({
-                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
-                    padding: theme.spacing.xl,
-                    borderRadius: theme.radius.md,
-                    cursor: "pointer",
-
-                    "&:hover": {
-                        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1],
-                    },
-                })}
-            >
-                <Divider />
-                {isMobile &&
-                    <div
-                        style={{
-                            paddingTop: '20px',
-                            boxSizing: 'content-box',
-                        }}>
+                <Group position="center">
+                    <div>
+                        <Title align="center">{countStats.num_photos}</Title>
                         <Group position="center">
-                            <Button color="Blue"
-                                style={{
-                                    height: "120px",
-                                    width: "120px",
-                                    borderRadius: "50%",
-                                    wordWrap: "break-word",
-                                    // whitespace:"normal",
-                                }}
-                                onClick={handleTripButton}>
-                                Create Trip
+                            <Button variant="subtle" color="dark" leftIcon={<Photo size={20} />}>
+                                {t("countstats.photos")}
                             </Button>
                         </Group>
                     </div>
+
+                    <div>
+                        <Title align="center">{countStats.num_people}</Title>
+                        <Group position="center">
+                            <Button variant="subtle" color="dark" leftIcon={<Users size={20} />}>
+                                {t("people")}
+                            </Button>
+                        </Group>
+                    </div>
+
+                    <div>
+                        <Title align="center">{countStats.num_faces}</Title>
+                        <Group position="center">
+                            <Button variant="subtle" color="dark" leftIcon={<FaceId size={20} />}>
+                                {t("faces")}
+                            </Button>
+                        </Group>
+                    </div>
+
+                    <div>
+                        <Title align="center">{countStats.num_albumauto}</Title>
+                        <Group position="center">
+                            <Button variant="subtle" color="dark" leftIcon={<SettingsAutomation size={20} />}>
+                                {t("events")}
+                            </Button>
+                        </Group>
+                    </div>
+
+                    <div>
+                        <Title align="center">{countStats.num_albumdate}</Title>
+                        <Group position="center">
+                            <Button variant="subtle" color="dark" leftIcon={<Calendar size={20} />}>
+                                {t("days")}
+                            </Button>
+                        </Group>
+                    </div>
+                </Group >
+
+                {/* <CountStats /> */}
+                {/* <Divider hidden /> */}
+                <Divider />
+                {isMobile &&
+                    <Box
+                        sx={theme => ({
+                            backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+                            padding: theme.spacing.xl,
+                            borderRadius: theme.radius.md,
+                            cursor: "pointer",
+
+                            "&:hover": {
+                                backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1],
+                            },
+                        })}
+                    >
+                        <Divider />
+
+                        <div
+                            style={{
+                                paddingTop: '20px',
+                                boxSizing: 'content-box',
+                            }}>
+                            <Group position="center">
+                                <Button color="Blue"
+                                    style={{
+                                        height: "120px",
+                                        width: "120px",
+                                        borderRadius: "50%",
+                                        wordWrap: "break-word",
+                                        // whitespace:"normal",
+                                    }}
+                                    onClick={handleTripButton}>
+                                    Create Trip
+                                </Button>
+                            </Group>
+                        </div>
+
+                        {click &&
+                            <Modal
+                                zIndex={1500}
+                                opened={click}
+                                title={<Title>Create New Trip</Title>}
+                                onClose={() => {
+                                    setClick(false);
+                                }}
+                            >
+                                <Stack>
+                                    <Group>
+                                        <Text color="dimmed">Trip Name:</Text>
+                                        <input id="Trip Name" type="text" />
+                                    </Group>
+                                    <Group>
+                                        <Text color="dimmed">Select Date:</Text>
+                                        <DatePicker
+                                            selected={date}
+                                            onChange={(date) => setDate(date)}
+                                        />
+                                        {/* 
+                                        <>
+                                            <button onClick={handleClick} type='button' color="Blue">
+                                                {moment(date).format("MMM Do YYYY, dddd")}
+                                            </button>
+                                            {isOpen && (
+                                                <DatePicker selected={date} onChange={handleChange} inline />
+                                            )}
+                                        </> */}
+                                    </Group>
+                                    <Group>
+
+                                    </Group>
+                                    <Button
+                                        // onClick={() => {
+                                        //     dispatch(
+                                        //         createNewUserAlbum(
+                                        //             newAlbumTitle,
+                                        //             selectedImages.map(i => i.id)
+                                        //         )
+                                        //     );
+                                        //     onRequestClose();
+                                        //     setNewAlbumTitle("");
+                                        // }}
+                                        // disabled={albumsUserList
+                                        //     .map(el => el.title.toLowerCase().trim())
+                                        //     .includes(newAlbumTitle.toLowerCase().trim())}
+                                        type="submit"
+                                    >
+                                        {t("modalalbum.create")}
+                                    </Button>
+                                </Stack>
+                            </Modal>
+                        }
+                    </Box>
                 }
-                {click &&
+                <Group>
+                    {/* <Divider /> */}
+                    <MapSearch size={35} />
+                    <Title order={2}>Trip Cards</Title>
+                </Group>
+
+            </Stack >
+            <div style={{
+                margin: "0 auto",
+                padding: "40px",
+                width: "100%",
+                // color: "#333",
+                // background: "#419be0",
+
+            }}>
+                <Slider {...carouselSettings}>
+                    <div>
+                        <img
+                            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                            src='https://www.technipages.com/wp-content/uploads/2020/12/Measure-distance-in-Google-Maps.jpg'
+                            alt=""
+                            onClick={handleClickCard} />
+                    </div>
+                    <div>
+                        <img
+                            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                            src='https://www.myrouteonline.com/wp-content/uploads/2018/05/Food-Delivery-e1527422030523.jpg'
+                            alt=""
+                            onClick={handleClickCard} />
+                    </div>
+                    <div>
+                        <img
+                            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                            src='https://i.stack.imgur.com/RfbqG.png'
+                            alt=""
+                            onClick={handleClickCard} />
+                    </div>
+                    <div>
+                        <img
+                            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                            src='https://developers.google.com/maps/images/landing/hero_directions_api.png'
+                            alt=""
+                            onClick={handleClickCard} />
+                    </div>
+                </Slider>
+                {clickCard &&
                     <Modal
                         zIndex={1500}
-                        opened={click}
-                        title={<Title>Create New Trip</Title>}
+                        opened={clickCard}
+                        title={<Title>Clicked Card</Title>}
                         onClose={() => {
-                            setClick(false);
+                            setClickCard(false);
                         }}
                     >
-                        <Stack>
-                            <Group>
-                                <Text color="dimmed">Trip Name:</Text>
-                                <input id="Trip Name" type="text" />
-                            </Group>
-                            <Group>
-                                <Text color="dimmed">Select Date:</Text>
-                                <DatePicker
-                                    selected={date}
-                                    onChange={(date) => setDate(date)}
-                                />
-
-                                {/* <>
-                                    <button onClick={handleClick} type='button'>
-                                        {date}
-                                    </button>
-                                    {isOpen && (
-                                        <DatePicker selected={date} onChange={handleChange} inline />
-                                    )}
-                                </> */}
-                            </Group>
-                            <Button
-                                // onClick={() => {
-                                //     dispatch(
-                                //         createNewUserAlbum(
-                                //             newAlbumTitle,
-                                //             selectedImages.map(i => i.id)
-                                //         )
-                                //     );
-                                //     onRequestClose();
-                                //     setNewAlbumTitle("");
-                                // }}
-                                // disabled={albumsUserList
-                                //     .map(el => el.title.toLowerCase().trim())
-                                //     .includes(newAlbumTitle.toLowerCase().trim())}
-                                type="submit"
-                            >
-                                {t("modalalbum.create")}
-                            </Button>
-
-                        </Stack>
-                    </Modal>
-                }
-
-
-            </Box>
-
-
-        </Stack >
+                    </Modal>}
+            </div>
+            {/* <JourneyMap /> */}
+        </div >
     );
 };
